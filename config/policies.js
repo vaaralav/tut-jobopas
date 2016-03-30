c382ad120bf7c882d15894dcc95b2d4a627678e6
Ville Vaarala <ville.l.vaarala@gmail.com>
2016-03-30 22:36:37 +0300

    User Bearer Auth only

--- config/policies.js
+++ config/policies.js
@@ -56,7 +56,7 @@ module.exports.policies = {
     '*' : ['passport']
   },
   'UserController' : {
-      ' * ': ['passport','bearerAuth']
+      ' * ': ['bearerAuth']
   },
   'MainController' : ['passport'],
 };
