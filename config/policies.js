9225e161b3b17c8b40b2dfa15fe1bc6f8ad16e4b
Ville Vaarala <ville.l.vaarala@gmail.com>
2016-03-30 23:37:03 +0300

    Fix userController policy

--- config/policies.js
+++ config/policies.js
@@ -56,7 +56,7 @@ module.exports.policies = {
     '*' : ['passport']
   },
   'UserController' : {
-      ' * ': ['bearerAuth']
+      '*': ['passport', 'bearerAuth']
   },
   'MainController' : ['passport'],
 };
