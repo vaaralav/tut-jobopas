0af8e87b67784192a9070e5e96466f0fa65e44c2
Ville Vaarala <ville.l.vaarala@gmail.com>
2016-03-30 23:37:36 +0300

    Start editing tags

--- assets/js/tags.js
+++ assets/js/tags.js
@@ -15,3 +15,23 @@ $("#tag-form").submit(function(e) {
   })
   return false;
 })
+
+$(".tags-list__tag__buttons__edit").click(function() {
+  var tag = $(this).parents(".tags-list__tag");
+  // Store the old value and remove the label
+  var oldVal = $(tag).find(".tags-list__tag__name").text();
+  $(tag).find(".tags-list__tag__name").remove()
+
+  // Insert text input
+  $(tag).append('<input type="text" value=' + oldVal + '/>');
+
+  $(this).removeClass(".tags-list__tag__buttons__edit")
+    .addClass(".tags-list__tag__buttons__save")
+    .text("Tallenna");
+
+  $(this).siblings('.tags-list__tag__buttons__remove')
+    .removeClass('.tags-list__tag__buttons__remove')
+    .addClass('.tags-list__tag__buttons__cancel')
+    .text("Peru");
+
+})
