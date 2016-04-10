83e18bfb2a0e4cc5ca098c3c626a9a2abcb9973f
Ville Vaarala <ville.l.vaarala@gmail.com>
2016-04-10 23:53:37 +0300

    Add Bearer Token info to the front page

--- api/controllers/MainController.js
+++ api/controllers/MainController.js
@@ -1,17 +1,21 @@
 var MainController =  {
   index : function index(req, res) {
     var user = (req.user ? req.user : false);
-    console.log(user);
+
     var tags = {};
     if(user) {
-      Tag.find()
-      .sort("id ASC")
-      .exec(function(err, tags) {
+      User.findOne(user.id).populateAll().exec(function( err, user) {
         if(err) return res.negotiate(err);
-        return res.view('main/index', {user: user, tags: tags});
+        console.log(user);
+        Tag.find()
+        .sort("id ASC")
+        .exec(function(err, tags) {
+          if(err) return res.negotiate(err);
+          return res.view('main/index', {user: user, tags: tags});
+        });
       });
     } else {
-      return res.view('main/index', {user: user});
+      return res.view('main/index');
     }
   }
 }
